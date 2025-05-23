a2a9b16cc2792f20ff08efaf9e567dae58a9aeb7
Akbar Putra Wiratama <148621758+AkbarPutraWiratama@users.noreply.github.com>
2025-05-23 14:54:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <title>Landing Page - Tugas & Latihan</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-</head>
 <body>
   <div class="container mt-5">
     <h1 class="mb-4">Daftar Isi Folder</h1>
